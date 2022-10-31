9b603e286fc2bcc1c103c6376783dfc71a99d6cf
sarvaev_iliya <i.sarvaev@taxi-master.com>
2022-10-31 23:43:32 +0400

    Initial Commit

---
+++ task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task2
+//
+//  Created by Volosandro on 31.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
